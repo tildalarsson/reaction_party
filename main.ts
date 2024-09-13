7560ab4c4c8e7eff64875a2448fdc60294d02b72
tildalarsson <tilda.larsson.9698@student.uu.se>
2024-09-13 11:08:55 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,58 @@
+input.onButtonPressed(Button.A, function () {
+    if (Motor) {
+        radio.sendValue(control.deviceName(), 1)
+        Motor = false
+    }
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    if (Motor) {
+        radio.sendValue(control.deviceName(), 5)
+        Motor = false
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    if (Motor) {
+        radio.sendValue(control.deviceName(), 3)
+        Motor = false
+    }
+})
+radio.onReceivedString(function (receivedString) {
+    if (receivedString == "Ny runda!") {
+        Motor = true
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if (Motor) {
+        radio.sendValue(control.deviceName(), 2)
+        Motor = false
+    }
+})
+input.onGesture(Gesture.Shake, function () {
+    if (Motor) {
+        radio.sendValue(control.deviceName(), 4)
+        Motor = false
+    }
+})
+input.onGesture(Gesture.TiltRight, function () {
+    if (Motor) {
+        radio.sendValue(control.deviceName(), 6)
+        Motor = false
+    }
+})
+radio.onReceivedValue(function (name, value) {
+    if (name == control.deviceName()) {
+        if (value == 111) {
+            basic.showString("Rätt!")
+            Poängställning += 1
+        } else {
+            basic.showString("Fel!")
+        }
+    }
+})
+let Motor = false
+let Poängställning = 0
+Motor = true
+basic.forever(function () {
+    radio.setGroup(18)
+    basic.showNumber(Poängställning)
+})
